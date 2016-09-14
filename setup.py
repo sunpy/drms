15dbb4e5117b5f48c0807d1379c5a4822a217860
Kolja Glogowski <kolja@pixie.de>
2016-09-14 19:34:34 +0200

    Updated setup.py, setuptools only, reST conversion
    Dropped support for plain distutils and added (optional) Markdown
    to reST conversion of README.md using pypandoc.

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python
-import os, re
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
+import re
+from setuptools import setup
 
 NAME = 'drms'
-DESCRIPTION = 'HMI, AIA and MDI data access using the JSOC DRMS.'
-LONG_DESCRIPTION = open('README.md').read()
+DESCRIPTION = 'HMI, AIA and MDI data access using the JSOC DRMS'
 AUTHOR = 'Kolja Glogowski'
 AUTHOR_EMAIL = '"Kolja Glogowski" <kolja@pixie.de>'
 URL = 'https://github.com/kbg/drms'
 LICENSE = 'MIT'
+
 
 # Read version string from __init__.py without importing the drms package, to
 # prevent an import error in case numpy, pandas or six are not installed yet.
@@ -20,10 +16,18 @@ m = re.search(r"""^\s*__version__\s*=\s*["'](.+)["']\s*$""",
               open('drms/__init__.py').read(), re.MULTILINE)
 VERSION = m.group(1)
 
-# For distutils builds: Make sure the MANIFEST file ist generated from
-# MANIFEST.in by removing the old file.
-if os.path.exists('MANIFEST'):
-    os.remove('MANIFEST')
+
+# The readme file uses Markdown, which does not seem to be supported by PyPI.
+# To solve this problem, we try to convert the README.md to reStructuredText
+# using pypandoc.
+try:
+    import pypandoc
+    LONG_DESCRIPTION = pypandoc.convert_file('README.md', 'rst')
+except:
+    # Fall back using the original Markdown text.
+    print('Warning: Pypandoc not found. Skipping README.md conversion.')
+    LONG_DESCRIPTION = open('README.md').read()
+
 
 setup(name=NAME,
       version=VERSION,
@@ -34,16 +38,21 @@ setup(name=NAME,
       url=URL,
       license=LICENSE,
       packages=['drms'],
-      install_requires=['numpy>=1.9.0', 'pandas>=0.14.1', 'six>=1.8.0'],
+      install_requires=[
+          'numpy>=1.9.0',
+          'pandas>=0.14.1',
+          'six>=1.8.0'],
       classifiers=[
-          'Development Status :: 5 - Production/Stable',
           'Intended Audience :: Developers',
           'Intended Audience :: Science/Research',
           'License :: OSI Approved :: MIT License',
           'Operating System :: OS Independent',
           'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
           'Topic :: Scientific/Engineering :: Astronomy'],
+      platforms='any'
       )
