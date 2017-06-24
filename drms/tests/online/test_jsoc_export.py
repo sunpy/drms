9adba75b08a8e1673c8e41076f30e7e55b65b5f2
Nitin Choudhary <nitin.iitkgp23@gmail.com>
2017-06-24 18:16:03 +0530

    Added export tests

--- drms/tests/online/test_jsoc_export.py
+++ drms/tests/online/test_jsoc_export.py
@@ -31,3 +31,38 @@ def test_export_asis_basic(jsoc_client_export, method):
     for url in r.urls.url:
         assert (url.endswith('mean.fits') or
                 url.endswith('power.fits'))
+
+
+@pytest.mark.jsoc
+@pytest.mark.export
+def test_export_fits_basic(jsoc_client_export):
+    r = jsoc_client_export.export(
+        'hmi.sharp_720s[4864][2014.11.30_00:00_TAI]{continuum, magnetogram}',
+        protocol='fits', method='url', requestor=False)
+
+    assert isinstance(r, drms.ExportRequest)
+    assert r.wait(timeout=60)
+    assert r.has_succeeded()
+    assert r.protocol == 'fits'
+    assert len(r.urls) == 2    # 1 file per segment
+
+    for record in r.urls.record:
+        record = record.lower()
+        assert record.startswith('hmi.sharp_720s[4864]')
+        assert record.endswith('2014.11.30_00:00:00_tai]')
+
+    for filename in r.urls.filename:
+        assert (filename.endswith('continuum.fits') or
+                filename.endswith('magnetogram.fits'))
+
+    for url in r.urls.url:
+        assert (url.endswith('continuum.fits') or
+                url.endswith('magnetogram.fits'))
+
+
+@pytest.mark.jsoc
+@pytest.mark.export
+def test_export_email(jsoc_client):
+    with pytest.raises(ValueError):
+        keys = jsoc_client.export(
+            'hmi.v_45s[2016.04.01_TAI/1d@6h]{Dopplergram}')
