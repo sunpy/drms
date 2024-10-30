64513bcb197659143d5dbafa478212c214448223
Stuart Mumford <stuart@cadair.com>
2024-10-30 16:14:33 +0000

    Update cruft with batchpr

--- drms/_dev/scm_version.py
+++ drms/_dev/scm_version.py
@@ -5,6 +5,13 @@ from pathlib import Path
 try:
     from setuptools_scm import get_version
 
+<<<<<<<
+=======
+    version = get_version(root=Path('../..'), relative_to=__file__)
+except ImportError:
+    raise
+except Exception as e:
+>>>>>>>
     version = get_version(root=Path("..") / "..", relative_to=__file__)
 except ImportError as e:
     raise ImportError("setuptools_scm not installed") from e
