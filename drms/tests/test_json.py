a1b1e066bb7e5b28d72b128839bcf133ef76892c
Nabil Freij <nabil.freij@gmail.com>
2025-01-30 13:56:56 -0800

    Add test

--- drms/tests/test_json.py
+++ drms/tests/test_json.py
@@ -1,7 +1,9 @@
+from unittest.mock import patch
+
 import pytest
 
 from drms.client import Client
-from drms.json import JsocInfoConstants
+from drms.json import HttpJsonRequest, JsocInfoConstants
 
 
 @pytest.mark.remote_data()
@@ -10,3 +12,17 @@ def test_jsocinfoconstants():
     assert JsocInfoConstants.all == "**ALL**"
     client = Client()
     client.query("hmi.synoptic_mr_720s[2150]", key=JsocInfoConstants.all, seg="synopMr")
+
+
+def test_request_headers():
+    with patch("drms.json.urlopen") as mock:
+        mock_response = mock.return_value
+        mock_response.read.return_value = b'{"key": "value"}'
+        mock_response.__enter__.return_value = mock_response
+        HttpJsonRequest("http://example.com", "latin1")
+
+    actual_request = mock.call_args[0][0]
+    assert actual_request.headers["User-agent"]
+    assert "drms/" in actual_request.headers["User-agent"]
+    assert "python/" in actual_request.headers["User-agent"]
+    assert actual_request.full_url == "http://example.com"
