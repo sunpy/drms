af0ad6004c0dba074ea1eb82673f580a50006e17
SunPyBot <sunpybot@users.noreply.github.com>
2026-09-09 11:01:33 +0000

    Run cruft update from package template

--- drms/version.py
+++ drms/version.py
@@ -10,7 +10,14 @@ except Exception:  # NOQA: BLE001
     import warnings
 
     warnings.warn(
+<<<<<<<
         f'could not determine {__name__.split(".")[0]} package version; this indicates a broken installation',
+=======
+        f"could not determine {__name__.split('.')[0]} package version; this indicates a broken installation",
+        stacklevel=2,
+    )
+    del warnings
+>>>>>>>
 <<<<<<<
         stacklevel=3,
 =======
